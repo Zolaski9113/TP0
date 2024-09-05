6bb2eded7c5192aa2d22e41f5057705a0832975d
Zolaski9113 <wajdi.gherairi1@gmail.com>
2024-09-05 19:26:54 -0400

    TP0 est fini

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
+print((rayon)**2 * math.pi)
 
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print("Wajdi Gherairi")
